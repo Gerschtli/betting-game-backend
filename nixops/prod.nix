52a4e99bec82f99a85ccc05d881f1cde0f77b076
Tobias Happ <tobias.happ@gmx.de>
2019-02-17 23:35:05 +0100

    Add production deployment

---
+++ nixops/prod.nix
@@ -0,0 +1,44 @@
+let
+  app = import ./app.nix;
+in
+
+{
+  network.description = app.description;
+
+  bgb =
+    { config, lib, pkgs, ... }:
+    lib.mkMerge [
+      {
+        deployment = {
+          targetEnv = "container";
+          container.host = "app.betting-game";
+        };
+      }
+
+      {
+        systemd.services.flask-server = {
+          description = "Flask Server";
+          after = [ "network.target" ];
+          wantedBy = [ "multi-user.target" ];
+          serviceConfig = {
+            ExecStart =
+              let
+                package = import ../. { inherit pkgs; };
+                python = pkgs.python36.withPackages (ps:
+                  [ package ps.gunicorn ] ++ app.libraries ps
+                );
+              in
+                ''
+                  ${python}/bin/python -m gunicorn.app.wsgiapp \
+                    --bind '0.0.0.0:${toString app.port}' --workers 4 \
+                    app:app
+                '';
+            User = app.user;
+            Restart = "always";
+          };
+        };
+      }
+
+      app.config
+    ];
+}
