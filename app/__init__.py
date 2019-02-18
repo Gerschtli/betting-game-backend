ef004fd8ec104d3f83e9199d4b6684dc5d849ab5
Tobias Happ <tobias.happ@gmx.de>
2019-02-18 11:57:02 +0100

    Move config folder

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,10 @@ from flask import Flask, jsonify
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
+from .config import default as default_config
+
 app = Flask(__name__)
-app.config.from_object('config.default')
+app.config.from_object(default_config)
 app.config.from_envvar('APP_CONFIG_FILE')
 
 db = SQLAlchemy(app)
---
+++ app/config/cli.py
@@ -0,0 +1,1 @@
+SQLALCHEMY_DATABASE_URI = 'mysql://betting_game:testpw@192.168.56.101/betting_game'
