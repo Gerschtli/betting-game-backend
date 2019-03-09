2d8e2894995b525f9d77d3bb88fac6f1d710e60f
Tobias Happ <tobias.happ@gmx.de>
2019-03-09 17:54:12 +0100

    Add mypy linter

--- ci/lint.sh
+++ ci/lint.sh
@@ -5,3 +5,4 @@ pycodestyle                    app tests
 isort --check-only --recursive app tests
 pyflakes                       app tests
 flake8                         app tests
+mypy                           app tests
