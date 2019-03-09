f1fed66c8de4b53c38a3d695cf2ee77eada808a4
Tobias Happ <tobias.happ@gmx.de>
2019-03-09 15:03:44 +0100

    Add jsonschema library

--- nixops/app.nix
+++ nixops/app.nix
@@ -45,6 +45,51 @@ rec {
     '';
   };
 
+  pyrsistent = ps: ps.pyrsistent.overridePythonAttrs (old: rec {
+    pname = "pyrsistent";
+    version = "0.14.11";
+    src = ps.fetchPypi {
+      inherit pname version;
+      sha256 = "1qkh74bm296mp5g3r11lgsksr6bh4w1bf8pji4nmxdlfj542ga1w";
+    };
+  });
+
+  perf = ps: ps.buildPythonPackage rec {
+    pname = "perf";
+    version = "1.6.0";
+
+    src = ps.fetchPypi {
+      inherit pname version;
+      sha256 = "1vrv83v8rhyl51yaxlqzw567vz5a9qwkymk3vqvcl5sa2yd3mzgp";
+    };
+
+    propagatedBuildInputs = with ps; [ six ];
+
+    doCheck = false;
+    checkInputs = with ps; [ nose2 psutil ];
+    checkPhase = "nosetests";
+  };
+
+  jsonschema = ps: ps.jsonschema.overridePythonAttrs (old: rec {
+    version = "3.0.1";
+    src = ps.fetchPypi {
+      inherit (old) pname;
+      inherit version;
+      sha256 = "03g20i1xfg4qdlk4475pl4pp7y0h37g1fbgs5qhy678q9xb822hc";
+    };
+    doCheck = false;
+    buildInputs = old.buildInputs ++ [
+      ps.twisted
+      (perf ps)
+    ];
+    propagatedBuildInputs = old.propagatedBuildInputs ++ [
+      ps.attrs
+      ps.setuptools_scm
+      ps.six
+      (pyrsistent ps)
+    ];
+  });
+
   libraries = ps: [
     ps.flask
     ps.flask_migrate
@@ -53,5 +98,6 @@ rec {
     ps.mysqlclient
     ps.passlib
     (flask-jwt-extended ps)
+    (jsonschema ps)
   ];
 }
