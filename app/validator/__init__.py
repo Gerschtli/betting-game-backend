596f0eabc6c7441f42da86ce32ed06112792b8ac
Tobias Happ <tobias.happ@gmx.de>
2019-03-17 16:48:18 +0100

    Use wrapt to build better decorators

--- app/validator/__init__.py
+++ app/validator/__init__.py
@@ -1,7 +1,7 @@
-from functools import wraps
-from typing import Any, Callable, Dict, TypeVar
+from typing import Any, Callable, Dict, List, TypeVar
 
 import jsonschema
+import wrapt
 from flask import request
 
 from ..errors import InputValidationError, SchemaValidationError
@@ -11,21 +11,19 @@ RT = TypeVar('RT')
 
 
 def validate_input(schema: Dict[str, Matcher]) -> Callable[[Callable[..., RT]], Callable[..., RT]]:
-    def wrapper(fn: Callable[..., RT]) -> Callable[..., RT]:
-        @wraps(fn)
-        def decorated(*args: Any, **kwargs: Any) -> RT:
-            errors = []
-            data = request.get_json()
+    @wrapt.decorator
+    def wrapper(wrapped: Callable[..., RT], instance: Any, args: List[Any],
+                kwargs: Dict[str, Any]) -> RT:
+        errors = []
+        data = request.get_json()
 
-            for key in schema:
-                errors.extend(schema[key].validate(data[key], key))
+        for key in schema:
+            errors.extend(schema[key].validate(data[key], key))
 
-            if errors:
-                raise InputValidationError(errors)
+        if errors:
+            raise InputValidationError(errors)
 
-            return fn(*args, **kwargs)
-
-        return decorated
+        return wrapped(*args, **kwargs)
 
     return wrapper
 
@@ -36,16 +34,14 @@ def validate_schema(schema: Dict[str, Any]) -> Callable[[Callable[..., RT]], Cal
 
     validator = jsonschema.validators.Draft7Validator(schema)
 
-    def wrapper(fn: Callable[..., RT]) -> Callable[..., RT]:
-        @wraps(fn)
-        def decorated(*args: Any, **kwargs: Any) -> RT:
-            errors = list(validator.iter_errors(request.get_json()))
+    @wrapt.decorator
+    def wrapper(wrapped: Callable[..., RT], instance: Any, args: List[Any],
+                kwargs: Dict[str, Any]) -> RT:
+        errors = list(validator.iter_errors(request.get_json()))
 
-            if errors:
-                raise SchemaValidationError(errors)
+        if errors:
+            raise SchemaValidationError(errors)
 
-            return fn(*args, **kwargs)
-
-        return decorated
+        return wrapped(*args, **kwargs)
 
     return wrapper
