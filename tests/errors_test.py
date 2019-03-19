209688ef43c257180ff842866e0c0ad18167d3a5
Tobias Happ <tobias.happ@gmx.de>
2019-03-19 20:28:53 +0100

    Add tests for register_error_handler

--- tests/errors_test.py
+++ tests/errors_test.py
@@ -1,6 +1,12 @@
-from jsonschema import ValidationError
+from http import HTTPStatus
+from typing import NoReturn
 
-from app.errors import InputValidationError, SchemaValidationError
+from flask import Flask, Response
+from jsonschema import ValidationError
+from sqlalchemy.exc import SQLAlchemyError
+from werkzeug.exceptions import NotFound
+
+from app.errors import InputValidationError, SchemaValidationError, register_error_handler
 
 
 class TestInputValidationError(object):
@@ -34,3 +40,51 @@ class TestSchemaValidationError(object):
 
     def test_subclass(self) -> None:
         assert issubclass(SchemaValidationError, Exception)
+
+
+class TestRegisterErrorHandler(object):
+    def test_database_error(self) -> None:
+        response = self._get_response(SQLAlchemyError())
+
+        assert response.data == b'{"message":"Internal Server Error"}\n'
+        assert response.status_code == HTTPStatus.INTERNAL_SERVER_ERROR
+
+    def test_input_validation_error(self) -> None:
+        response = self._get_response(InputValidationError([{'abc': 'def'}]))
+
+        assert response.data == b'[{"abc":"def"}]\n'
+        assert response.status_code == HTTPStatus.BAD_REQUEST
+
+    def test_not_found(self) -> None:
+        response = self._get_response(NotFound())
+
+        assert response.data == b'{"message":"Not Found"}\n'
+        assert response.status_code == HTTPStatus.NOT_FOUND
+
+    def test_schema_validation_error(self) -> None:
+        response = self._get_response(
+            SchemaValidationError([
+                ValidationError("msg1"),
+                ValidationError("msg2"),
+            ]))
+
+        assert response.data == b'["msg1","msg2"]\n'
+        assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
+
+    def _get_response(self, exception: Exception) -> Response:
+        app = Flask(__name__)
+        app.config['TESTING'] = True
+        register_error_handler(app)
+
+        @app.route('/')
+        def test_route() -> NoReturn:
+            raise exception
+
+        client = app.test_client()
+
+        response = client.get('/')
+
+        assert response.is_json
+        assert response.headers.get('Content-Type') == 'application/json'
+
+        return response
