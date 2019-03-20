ab8333c82c24c2ccb742e1c3dbb51344a62c3e8a
Tobias Happ <tobias.happ@gmx.de>
2019-03-20 17:49:50 +0100

    Add test for check_if_token_in_blacklist

--- app/jwt.py
+++ app/jwt.py
@@ -2,7 +2,7 @@ from typing import Any, Dict
 
 from flask_jwt_extended import JWTManager
 
-from .models import Token
+from . import models
 
 jwt = JWTManager()
 
@@ -10,4 +10,4 @@ jwt = JWTManager()
 @jwt.token_in_blacklist_loader
 def check_if_token_in_blacklist(decrypted_token: Dict[str, Any]) -> bool:
     jti = decrypted_token['jti']
-    return Token.is_jti_blacklisted(jti)
+    return models.Token.is_jti_blacklisted(jti)
