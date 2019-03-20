d09c0941fe5418c96bc7962f473b8f8d8b7bf549
Tobias Happ <tobias.happ@gmx.de>
2019-03-20 14:15:18 +0100

    Enable possibility to mock decorator behaviour

--- app/validator/__init__.py
+++ app/validator/__init__.py
@@ -10,34 +10,44 @@ from .matcher import Matcher
 RT = TypeVar('RT')
 
 
+def _validate_input(schema: Dict[str, Matcher], wrapped: Callable[..., RT], args: List[Any],
+                    kwargs: Dict[str, Any]) -> RT:
+    errors = []
+    data = request.get_json()
+
+    for key in schema:
+        errors.extend(schema[key].validate(data[key], key))
+
+    if errors:
+        raise InputValidationError(errors)
+
+    return wrapped(*args, **kwargs)
+
+
 def validate_input(schema: Dict[str, Matcher]) -> Callable[[Callable[..., RT]], Callable[..., RT]]:
     @wrapt.decorator
     def wrapper(wrapped: Callable[..., RT], instance: Any, args: List[Any],
                 kwargs: Dict[str, Any]) -> RT:
-        errors = []
-        data = request.get_json()
-
-        for key in schema:
-            errors.extend(schema[key].validate(data[key], key))
-
-        if errors:
-            raise InputValidationError(errors)
-
-        return wrapped(*args, **kwargs)
+        return _validate_input(schema, wrapped, args, kwargs)
 
     return wrapper
+
+
+def _validate_schema(schema: Dict[str, Any], wrapped: Callable[..., RT], args: List[Any],
+                     kwargs: Dict[str, Any]) -> RT:
+    validator = validators.Draft7Validator(schema)
+    errors = list(validator.iter_errors(request.get_json()))
+
+    if errors:
+        raise SchemaValidationError(errors)
+
+    return wrapped(*args, **kwargs)
 
 
 def validate_schema(schema: Dict[str, Any]) -> Callable[[Callable[..., RT]], Callable[..., RT]]:
     @wrapt.decorator
     def wrapper(wrapped: Callable[..., RT], instance: Any, args: List[Any],
                 kwargs: Dict[str, Any]) -> RT:
-        validator = validators.Draft7Validator(schema)
-        errors = list(validator.iter_errors(request.get_json()))
-
-        if errors:
-            raise SchemaValidationError(errors)
-
-        return wrapped(*args, **kwargs)
+        return _validate_schema(schema, wrapped, args, kwargs)
 
     return wrapper
