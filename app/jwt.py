a948181c1aadc8d83330a25f318ca120866a450e
Tobias Happ <tobias.happ@gmx.de>
2019-05-05 13:42:06 +0200

    Add jwt user_claims_loader

--- app/jwt.py
+++ app/jwt.py
@@ -7,6 +7,15 @@ from . import models
 jwt = JWTManager()
 
 
+# Create a function that will be called whenever create_access_token
+# is used. It will take whatever object is passed into the
+# create_access_token method, and lets us define what custom claims
+# should be added to the access token.
+@jwt.user_claims_loader
+def add_claims_to_access_token(user: models.User) -> Dict[str, bool]:
+    return {'is_admin': user.is_admin}
+
+
 @jwt.token_in_blacklist_loader
 def check_if_token_in_blacklist(decrypted_token: Dict[str, Any]) -> bool:
     jti = decrypted_token['jti']
--- tests/jwt_test.py
+++ tests/jwt_test.py
@@ -1,15 +1,28 @@
 from unittest.mock import Mock, patch
 
+import pytest
 from flask_jwt_extended import JWTManager
 
-from app.jwt import check_if_token_in_blacklist, jwt, user_identity_lookup
+from app.jwt import (add_claims_to_access_token, check_if_token_in_blacklist, jwt,
+                     user_identity_lookup)
 from app.models import User
 
 
 def test_init() -> None:
     assert isinstance(jwt, JWTManager)
     assert jwt._token_in_blacklist_callback == check_if_token_in_blacklist
+    assert jwt._user_claims_callback == add_claims_to_access_token
     assert jwt._user_identity_callback == user_identity_lookup
+
+
+@pytest.mark.parametrize(
+    'is_admin',
+    [True, False],
+)
+def test_add_claims_to_access_token(is_admin: bool) -> None:
+    user = User(is_admin=is_admin)
+
+    assert add_claims_to_access_token(user) == {'is_admin': is_admin}
 
 
 @patch('app.models.Token')
