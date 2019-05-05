f4a0a47b18dea8d0782a55d88917b4a9d17ed594
Tobias Happ <tobias.happ@gmx.de>
2019-05-05 16:17:50 +0200

    Add schemas for login and invitation

--- app/validator/schemas.py
+++ app/validator/schemas.py
@@ -1,4 +1,19 @@
-USER = {
+INVITATION = {
+    '$schema': 'http://json-schema.org/draft-07/schema#',
+    'additionalProperties': False,
+    'type': 'object',
+    'required': ['email', 'is_admin'],
+    'properties': {
+        'email': {
+            'type': 'string',
+        },
+        'is_admin': {
+            'type': 'boolean',
+        },
+    },
+}
+
+LOGIN = {
     '$schema': 'http://json-schema.org/draft-07/schema#',
     'additionalProperties': False,
     'type': 'object',
@@ -9,6 +24,27 @@ USER = {
         },
         'password': {
             'type': 'string',
+        },
+    },
+}
+
+USER = {
+    '$schema': 'http://json-schema.org/draft-07/schema#',
+    'additionalProperties': False,
+    'type': 'object',
+    'required': ['username', 'password', 'email', 'is_admin'],
+    'properties': {
+        'username': {
+            'type': 'string',
+        },
+        'password': {
+            'type': 'string',
+        },
+        'email': {
+            'type': 'string',
+        },
+        'is_admin': {
+            'type': 'boolean',
         },
     },
 }
--- tests/validator/schemas_test.py
+++ tests/validator/schemas_test.py
@@ -4,9 +4,23 @@ from app.validator import schemas
 
 
 def test_schemas() -> None:
-    assert len([schema for schema in dir(schemas) if schema.isupper()]) == 1
+    assert len([schema for schema in dir(schemas) if schema.isupper()]) == 3
 
-    assert schemas.USER == {
+    assert schemas.INVITATION == {
+        '$schema': 'http://json-schema.org/draft-07/schema#',
+        'additionalProperties': False,
+        'type': 'object',
+        'required': ['email', 'is_admin'],
+        'properties': {
+            'email': {
+                'type': 'string',
+            },
+            'is_admin': {
+                'type': 'boolean',
+            },
+        },
+    }
+    assert schemas.LOGIN == {
         '$schema': 'http://json-schema.org/draft-07/schema#',
         'additionalProperties': False,
         'type': 'object',
@@ -20,10 +34,32 @@ def test_schemas() -> None:
             },
         },
     }
+    assert schemas.USER == {
+        '$schema': 'http://json-schema.org/draft-07/schema#',
+        'additionalProperties': False,
+        'type': 'object',
+        'required': ['username', 'password', 'email', 'is_admin'],
+        'properties': {
+            'username': {
+                'type': 'string',
+            },
+            'password': {
+                'type': 'string',
+            },
+            'email': {
+                'type': 'string',
+            },
+            'is_admin': {
+                'type': 'boolean',
+            },
+        },
+    }
 
 
 def test_schema_validation() -> None:
     """
     Tests whether the schemas are valid. If not an jsonschema.exceptions.SchemaError is raised.
     """
+    validators.Draft7Validator.check_schema(schemas.INVITATION)
+    validators.Draft7Validator.check_schema(schemas.LOGIN)
     validators.Draft7Validator.check_schema(schemas.USER)
