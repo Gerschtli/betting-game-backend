ef5a028cbf547439777c1e5d6afdc265dfbddbab
Tobias Happ <tobias.happ@gmx.de>
2019-05-05 13:45:38 +0200

    Add admin_required decorator

--- app/resource.py
+++ app/resource.py
@@ -1,10 +1,47 @@
+from typing import Any, Callable, Dict, List, TypeVar
+
+import flask_jwt_extended
 import flask_restful
-from flask_jwt_extended import jwt_required
+import wrapt
+from werkzeug.exceptions import Forbidden
+
+RT = TypeVar('RT')
+
+
+@wrapt.decorator
+def admin_required(
+        wrapped: Callable[..., RT],
+        instance: Any,
+        args: List[Any],
+        kwargs: Dict[str, Any],
+) -> RT:
+    flask_jwt_extended.verify_jwt_in_request()
+    claims = flask_jwt_extended.get_jwt_claims()
+
+    if claims['is_admin']:
+        return wrapped(*args, **kwargs)
+
+    raise Forbidden()
 
 
 class Resource(flask_restful.Resource):  # type: ignore
     pass
 
 
+class AdminResource(Resource):
+    method_decorators = [admin_required]
+
+
 class AuthenticatedResource(Resource):
-    method_decorators = [jwt_required]
+    method_decorators = [flask_jwt_extended.jwt_required]
+
+
+class UserReadableResource(Resource):
+    method_decorators = {
+        'delete': [admin_required],
+        'get': [flask_jwt_extended.jwt_required],
+        'head': [flask_jwt_extended.jwt_required],
+        'patch': [admin_required],
+        'post': [admin_required],
+        'put': [admin_required],
+    }
--- tests/resource_test.py
+++ tests/resource_test.py
@@ -1,7 +1,89 @@
+from unittest.mock import Mock, patch
+
 import flask_restful
 from flask_jwt_extended import jwt_required
+from flask_jwt_extended.exceptions import UserClaimsVerificationError
+from werkzeug.exceptions import Forbidden
 
-from app.resource import AuthenticatedResource, Resource
+from app.resource import (AdminResource, AuthenticatedResource, Resource, UserReadableResource,
+                          admin_required)
+
+
+@patch('flask_jwt_extended.get_jwt_claims')
+@patch('flask_jwt_extended.verify_jwt_in_request')
+def test_admin_required(mock_verify_jwt_in_request: Mock, mock_get_jwt_claims: Mock) -> None:
+    called = False
+
+    @admin_required
+    def function(arg1: str, arg2: int) -> int:
+        nonlocal called
+        called = True
+
+        assert arg1 == 'bla'
+        assert arg2 == 342
+
+        return 17
+
+    mock_get_jwt_claims.return_value = {'is_admin': True}
+
+    result = function('bla', arg2=342)
+
+    mock_verify_jwt_in_request.assert_called_once_with()
+    mock_get_jwt_claims.assert_called_once_with()
+
+    assert result == 17
+
+
+@patch('flask_jwt_extended.get_jwt_claims')
+@patch('flask_jwt_extended.verify_jwt_in_request')
+def test_admin_required_when_not_admin(mock_verify_jwt_in_request: Mock,
+                                       mock_get_jwt_claims: Mock) -> None:
+    called = False
+
+    @admin_required
+    def function() -> None:
+        nonlocal called
+        called = True
+
+    mock_get_jwt_claims.return_value = {'is_admin': False}
+
+    exception = None
+    try:
+        function()
+    except Exception as e:
+        exception = e
+
+    mock_verify_jwt_in_request.assert_called_once_with()
+    mock_get_jwt_claims.assert_called_once_with()
+
+    assert exception is not None
+    assert isinstance(exception, Forbidden)
+
+
+@patch('flask_jwt_extended.get_jwt_claims')
+@patch('flask_jwt_extended.verify_jwt_in_request')
+def test_admin_required_with_invalid_jwt(mock_verify_jwt_in_request: Mock,
+                                         mock_get_jwt_claims: Mock) -> None:
+    called = False
+
+    @admin_required
+    def function() -> None:
+        nonlocal called
+        called = True
+
+    mock_verify_jwt_in_request.side_effect = UserClaimsVerificationError('error')
+
+    exception = None
+    try:
+        function()
+    except Exception as e:
+        exception = e
+
+    mock_verify_jwt_in_request.assert_called_once_with()
+    mock_get_jwt_claims.assert_not_called()
+
+    assert exception is not None
+    assert isinstance(exception, UserClaimsVerificationError)
 
 
 def test_resource() -> None:
@@ -9,7 +91,26 @@ def test_resource() -> None:
     assert Resource.method_decorators == []
 
 
+def test_admin_resource() -> None:
+    assert issubclass(AdminResource, flask_restful.Resource)
+    assert issubclass(AdminResource, Resource)
+    assert AdminResource.method_decorators == [admin_required]
+
+
 def test_authenticated_resource() -> None:
     assert issubclass(AuthenticatedResource, flask_restful.Resource)
     assert issubclass(AuthenticatedResource, Resource)
     assert AuthenticatedResource.method_decorators == [jwt_required]
+
+
+def test_user_readable_resource() -> None:
+    assert issubclass(UserReadableResource, flask_restful.Resource)
+    assert issubclass(UserReadableResource, Resource)
+    assert UserReadableResource.method_decorators == {
+        'delete': [admin_required],
+        'get': [jwt_required],
+        'head': [jwt_required],
+        'patch': [admin_required],
+        'post': [admin_required],
+        'put': [admin_required],
+    }
