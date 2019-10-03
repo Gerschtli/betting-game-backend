5bf2da48d609b046604fb538e5ac19ad0155a387
Tobias Happ <tobias.happ@gmx.de>
2019-10-03 11:49:32 +0200

    Move shell.nix overrides to overlay

--- nixops/nixpkgs.nix
+++ nixops/nixpkgs.nix
@@ -1,1 +1,61 @@
-import ((import <nixpkgs> { }).fetchzip (import ./nixpkgs-version.nix)) { }
+import ((import <nixpkgs> { }).fetchzip (import ./nixpkgs-version.nix)) {
+  config = { };
+  overlays = [
+    (self: super:
+      {
+        python36Packages = (super.python36Packages or {}) // {
+          flake8-quotes = super.python36Packages.buildPythonPackage rec {
+            pname = "flake8-quotes";
+            version = "2.1.0";
+
+            src = super.python36Packages.fetchPypi {
+              inherit pname version;
+              sha256 = "1v9l9454lxi1zyf1dwp9gkvvkr52dlyr91zv8s1z4wvqi1lgdfjx";
+            };
+
+            doCheck = false;
+            propagatedBuildInputs = [ self.python36Packages.flake8 ];
+          };
+
+          mypy = super.python36Packages.mypy.overridePythonAttrs (old: rec {
+            version = "0.730";
+
+            src = super.python36Packages.fetchPypi {
+              inherit (old) pname;
+              inherit version;
+              sha256 = "0ygqviby0i4i3k2mlnr08f07dxvkh5ncl17m14bg4w07x128k9s2";
+            };
+
+            propagatedBuildInputs = with self.python36Packages; [
+              typed-ast
+              psutil
+              mypy_extensions
+              typing-extensions
+            ];
+          });
+
+          typed-ast = super.python36Packages.typed-ast.overridePythonAttrs (old: rec {
+            version = "1.4.0";
+
+            src = super.fetchFromGitHub {
+              owner = "python";
+              repo = "typed_ast";
+              rev = version;
+              sha256 = "0l0hz809f7i356kmqkvfsaswiidb98j9hs9rrjnfawzqcbffzgyb";
+            };
+          });
+
+          typing-extensions = super.python36Packages.typing-extensions.overridePythonAttrs (old: rec {
+            version = "3.7.4";
+
+            src = super.python36Packages.fetchPypi {
+              inherit (old) pname;
+              inherit version;
+              sha256 = "15bx773a5zkk4hkwjl8nb5f8y5741vyyqb9q3jac6kxm1frk5mif";
+            };
+          });
+        };
+      }
+    )
+  ];
+}
--- shell.nix
+++ shell.nix
@@ -1,61 +1,6 @@
 with import ./nixops/nixpkgs.nix;
 
 let
-  mypy_extensions_ = python36Packages.buildPythonPackage rec {
-    pname = "mypy_extensions";
-    version = "0.4.1";
-
-    # Tests not included in pip package.
-    doCheck = false;
-
-    src = python36Packages.fetchPypi {
-      inherit pname version;
-      sha256 = "04h8brrbbx151dfa2cvvlnxgmb5wa00mhd2z7nd20s8kyibfkq1p";
-    };
-
-    propagatedBuildInputs = [ python36Packages.typing ];
-  };
-
-  typed-ast_ = python36Packages.typed-ast.overridePythonAttrs (old: rec {
-    version = "1.3.1";
-
-    src = python36Packages.fetchPypi {
-      inherit (old) pname;
-      inherit version;
-      sha256 = "095l1jfxk1k2z5sijbirg2s5a01r4ig3715zqa075xzf0zx8lvb0";
-    };
-  });
-
-  mypy_ = python36Packages.mypy.overridePythonAttrs (old: rec {
-    version = "0.700";
-
-    src = python36Packages.fetchPypi {
-      inherit (old) pname;
-      inherit version;
-      sha256 = "1zxfi5s9hxrz0hbaj4n513az17l44pxl80r62ipjc0bsmbcic2xi";
-    };
-
-    propagatedBuildInputs = [
-      python36Packages.lxml
-      python36Packages.psutil
-      mypy_extensions_
-      typed-ast_
-    ];
-  });
-
-  flake8-quotes = python36Packages.buildPythonPackage rec {
-    pname = "flake8-quotes";
-    version = "2.0.1";
-
-    src = python36Packages.fetchPypi {
-      inherit pname version;
-      sha256 = "0f10q2580bzxmr0lic42q32qbyf3aq42di91wyl04hrd8xmszj8h";
-    };
-
-    doCheck = false;
-    propagatedBuildInputs = [ python36Packages.flake8 ];
-  };
-
   app = import ./nixops/app.nix;
 in
 
@@ -68,7 +13,7 @@ mkShell {
       flake8-quotes
       git-crypt
       isort
-      mypy_
+      mypy
       python-language-server
     ]);
 
